4fb6a54a561a30ffb56425f6fbb1d43448d55db7
vaishalifarkade143 <vaishalifarkade143@gmail.com>
2023-08-31 13:16:41 +0530

    Login commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,24 +1,45 @@
-import React, { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const[isLoading,setIsLoading] = useState(true);
-    const [userToken,setUserToken]= useState(null);
-   
-    const login = () =>{
+    const [isLoading, setIsLoading] = useState(false);
+    const [userToken, setUserToken] = useState(null);
+
+    const login = () => {
+        setIsLoading(true);
         setUserToken('ioiojlkad');
+        AsyncStorage.setItem('userToken', 'ioiojlkad');
         setIsLoading(false);
     }
 
-    const logout = () =>{
+    const logout = () => {
+        setIsLoading(true);
         setUserToken(null);
+        AsyncStorage.removeItem('userToken')
         setIsLoading(false);
     }
+    const isLoggedIn = async () => {
+        try {
+            setIsLoading(true);
+            let userToken = await AsyncStorage.getItem('userToken');
+            setUserToken(userToken);
+            setIsLoading(false);
+
+        }
+        catch (e) {
+            console.log(`isLogged in error ${e}`);
+        }
+    }
+
+    useEffect(() => {
+      isLoggedIn();
+    }, [])
 
 
     return (
-        <AuthContext.Provider value={{login,logout,isLoading,userToken}}>
+        <AuthContext.Provider value={{ login, logout, isLoading, userToken }}>
             {children}
         </AuthContext.Provider>
     );
--- src/screens/Books.js
+++ src/screens/Books.js
@@ -9,4 +9,39 @@ const Books = () => {
   )
 }
 
-export default Books
+export default Books;
+        
+// import React, { useState } from 'react';
+// import { View, Text, Picker } from 'react-native';
+
+// const App = () => {
+//   const [selectedGenre, setSelectedGenre] = useState("Search By Genre");
+//   const genres = [
+//     "Search By Genre",
+//     "Art", "Biography", "Business", "Comics", "Contemporary", "Crime", "Fantasy",
+//     "Fiction", "Novels", "History", "Horror", "Humor and Comedy", "Music", "Mystery",
+//     "Nonfiction", "Philosophy", "Poetry", "Psychology", "Religion", "Romance",
+//     "Science", "Educational", "Civil service", "Mathematics", "Suspense",
+//     "Spirituality", "Sports", "Thriller", "Travel", "Economics", "Politics",
+//     "Atlas", "Banking", "Physics", "General Knowledge", "Story", "Polity",
+//     "Biology", "Chemistry", "UPSC"
+//   ];
+
+//   return (
+//     <View style={{ paddingHorizontal: 16 }}>
+//       <View style={{ marginVertical: 16 }}>
+        
+//         <Picker
+//           selectedValue={selectedGenre}
+//           onValueChange={(itemValue) => setSelectedGenre(itemValue)}
+//         >
+//           {genres.map((genre, index) => (
+//             <Picker.Item key={index} label={genre} value={genre} />
+//           ))}
+//         </Picker>
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default App;
