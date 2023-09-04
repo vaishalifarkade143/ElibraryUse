8a7d07224f42052957a6197f1e24f9775a50b4e3
vaishalifarkade143 <vaishalifarkade143@gmail.com>
2023-09-04 19:13:36 +0530

    loginNLogout button

--- src/screens/User.js
+++ src/screens/User.js
@@ -1,5 +1,5 @@
-import { View, Text,Image,TouchableOpacity } from 'react-native'
-import React ,{useContext} from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React, { useContext } from 'react'
 import Header from '../common/Header';
 import { AuthContext } from '../context/AuthContext';
 
@@ -7,10 +7,10 @@ import { AuthContext } from '../context/AuthContext';
 
 const User = ({ navigation }) => {
   const { logout } = useContext(AuthContext);
-  const {userInfo} = useContext(AuthContext);
-  const {userToken} = useContext(AuthContext);
+  const { userInfo } = useContext(AuthContext);
+  const { userToken } = useContext(AuthContext);
   return (
-    <View style={{ flex: 1,}}>
+    <View style={{ flex: 1, }}>
       <Header
         rightIcon={require('../images/Logoelibrary.png')}
         leftIcon={require('../images/menu.png')}
@@ -35,91 +35,94 @@ const User = ({ navigation }) => {
           <View >
             <Image
               source={require('../images/hero-brownElib.png')}
-              style={{ width: 180,
-                height: '90%'}}
+              style={{
+                width: 180,
+                height: '90%'
+              }}
               resizeMode='contain'
             />
-            
+
           </View>
-          <View  style={{flexDirection: 'column',}}>
-       
-          {/* <Text> Hello{userInfo.data.user.first_name} {userInfo.data.user.last_name}</Text>
+          <View style={{ flexDirection: 'column', }}>
+
+            {/* <Text> Hello{userInfo.data.user.first_name} {userInfo.data.user.last_name}</Text>
           <Text> {userInfo.data.user.email}</Text>
           <Text> {userInfo.data.user.phone}</Text>  */}
 
-           {userToken != null?
-        <View>
-          <Text> {userInfo.data.user.first_name} {userInfo.data.user.last_name}</Text>
-          <Text> {userInfo.data.user.email}</Text>
-          <Text> {userInfo.data.user.phone}</Text></View>
-        :null}
-         
-         
-          <TouchableOpacity
-                        style={{
-                            backgroundColor: '#c27b7f',
-                            alignItems: 'center',
-                            padding: 10,
-                            borderRadius: 5,
-                            width: '100%',
-                            height: 50,
-                            justifyContent: 'center',
-                            marginTop: 50
-                        }}
+            {userToken != null ?
+              <View>
+                <Text> {userInfo.data.user.first_name} {userInfo.data.user.last_name}</Text>
+                <Text> {userInfo.data.user.email}</Text>
+                <Text> {userInfo.data.user.phone}</Text></View>
+              : null}
 
-                        // {/* on login button click */}
-                        onPress={()=> {
-                            navigation.navigate('Loginnn');
-                        }}
-                       // disabled={!email || !password}
-                    >
-                        <Text style={{
-                            color: '#fff',
-                            fontWeight: '700',
-                            fontSize: 18
-                        }}>Login/Join</Text>
+{userToken === null ?
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#c27b7f',
+                alignItems: 'center',
+                padding: 10,
+                borderRadius: 5,
+                width: '100%',
+                height: 50,
+                justifyContent: 'center',
+                marginTop: 50
+              }}
 
-                    </TouchableOpacity>
-          
-      </View> 
-      
-      </View>
-    </View>
-    
+              // {/* on login button click */}
+              onPress={() => {
+                navigation.navigate('Loginnn');
+              }}
+            // disabled={!email || !password}
+            >
+              <Text style={{
+                color: '#fff',
+                fontWeight: '700',
+                fontSize: 18
+              }}>Login/Join</Text>
 
-    <View style={{bottom:0,justifyContent:'center',alignItems:'center'}}>
-    <TouchableOpacity
-                        style={{
-                            backgroundColor: '#c27b7f',
-                            alignItems: 'center',
-                            padding: 10,
-                            borderRadius: 5,
-                            width: '50%',
-                            height: 50,
-                            justifyContent: 'center',
-                           //bottom:0
-                        }}
+            </TouchableOpacity>:null}
 
-                        // {/* on login button click */}
-                      //   onPress={()=> {
-                      //     navigation.navigate('Home2');
-                      // }}
+          </View>
 
-                        onPress={()=> {logout()}}
-                       // disabled={!email || !password}
-                    >
-                        <Text style={{
-                            color: '#fff',
-                            fontWeight: '700',
-                            fontSize: 18
-                        }}>Logout</Text>
-
-                    </TouchableOpacity>
-    </View>
-
+        </View>
       </View>
 
-      );
+
+      <View style={{ bottom: 0, justifyContent: 'center', alignItems: 'center' }}>
+      {userToken != null ?
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#c27b7f',
+            alignItems: 'center',
+            padding: 10,
+            borderRadius: 5,
+            width: '50%',
+            height: 50,
+            justifyContent: 'center',
+            //bottom:0
+          }}
+
+          // {/* on login button click */}
+          //   onPress={()=> {
+          //     navigation.navigate('Home2');
+          // }}
+
+          onPress={() => { logout() }}
+        // disabled={!email || !password}
+        >
+          <Text style={{
+            color: '#fff',
+            fontWeight: '700',
+            fontSize: 18
+          }}>Logout</Text>
+
+        </TouchableOpacity>:null}
+      </View>
+
+    </View>
+
+  );
 };
 
-      export default User;
+export default User;
